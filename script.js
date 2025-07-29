f85a8a82112733d79292aeb4791c05cac8c8d448
Mohammedhee <alryanltjartaladwatalshyt@gmail.com>
2025-07-30 02:07:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function contact() {
+  alert("تواصل معنا على الرقم: 9999999999 أو زورنا في المنوفية، منوف، كفر سنجرج");
+}
+
+  function openContactForm() {
+    document.getElementById("contactFormPopup").style.display = "block";
+  }
+
+  function closeContactForm() {
+    document.getElementById("contactFormPopup").style.display = "none";
+  }
+
